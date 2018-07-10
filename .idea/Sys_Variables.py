4758427178aed826ea889e19574d32940f551439
sriharisrinivasan <sriharisrinivasan@rediffmail.com>
2018-07-10 22:29:25 +0530

    Initial commit

---
+++ .idea/Sys_Variables.py
@@ -0,0 +1,29 @@
+#Change os to "Android" if you want to run this from mobile
+# os = "Android"
+#Change os to "Windows" if you want to run this from Windows PC
+os = "Windows"
+Android_Path = '/storage/emulated/0/MIDI_Test'
+scale_list = ["Select Scale", "Major Scale", "Minor Scale", "Chromatic Scale"]
+pitch_list = ["Select Pitch","C", "Cs", "Df", "D", "Ds", "Ef", "E", "F", "Fs", "Gf", "G", "Gs", "Af", "A", "As", "Bf", "B"]
+instrument_list = ["Select Instrument", "Piano 1", "Piano 2", "Electric Piano", "Harpsichord", "Strings" ]
+scale = "MAJOR"
+pitch = "C"
+instrument = "Piano 1"
+chan0_inst = "Piano 1"
+chan1_inst = "Piano 1"
+chan2_inst = "Piano 1"
+chan3_inst = "Piano 1"
+IP_Address = "192.168.0.81"
+Port = 5000
+music_filename = ""
+channel = 0
+note = 60
+chord_type = 'MAJ'
+octave = 4
+duration = 1
+velocity = 100
+tempo = 88
+beatspermeasure = 4
+measure = 4
+no_of_tracks = 4
+channel_sel = 0
